3f6cb12009d483e7b965e79aa07d12dbf7ddeb42
The Mighty Quinn <quentin.jendrzewski@sotw.com>
2016-03-23 11:22:09 +0000

    Amended unit-tests Authentication has been removed from cve web service. The code is in place and can be used later.  Perhaps it would be better employed by passing credentials in header rather than in the request.

--- tests/webservices/CveRestTest.php
+++ tests/webservices/CveRestTest.php
@@ -42,24 +42,17 @@ class CveRestTest extends PHPUnit_Framework_TestCase
     /**
      * Executes the API
      * 
-     * @param array $p_params URL query parameters
+     * @param string $p_url Endpoint URL
      * 
      * @return string
      */
-    private function _executeApi(array $p_params)
+    private function _executeApi($p_url)
     {
-        $p_params = array_merge(
-            $p_params,
-            array(
-                'XDEBUG_SESSION_START' => 'netbeans-xdebug'
-            )
-        );
-        $url = \Zend_Registry::get('Config')->url->cve . "?" . http_build_query($p_params);
         $arrHeader = array( 
             'Accept: application/json'
         );
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_URL, $p_url);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
@@ -68,48 +61,31 @@ class CveRestTest extends PHPUnit_Framework_TestCase
     }
     
     /**
-     * Testing cve with invalid username
+     * Testing cve with a successful result
+     * This is a naughty test, because it assumes the legacy CVE records will remain unchanged and will retain 
+     * their same position in the data as new data is augmented to the source data CSV
      * 
      * @return void
      */
-    public function testInvalidUsername()
+    public function testGetCveByNumberSuccess()
     {
-        $response = $this->_executeApi(
-            array(
-                'pass' => 'me',
-                
-            )
-        );
+        $url = \Zend_Registry::get('Config')->url->cve . "/CVE-1999-0004?XDEBUG_SESSION_START=netbeans-xdebug";
+        $response = $this->_executeApi($url);
         $this->assertEquals(
             array(
-                'result' => 0,
-                'resultDescription' => 'Failure',
-                'reason' => \API\Services\Auth::INVALID_USERNAME_EMPTY,
-            ),
-            json_decode((string)$response, true)
-        );
-        
-    }
-    
-    /**
-     * Testing cve with invalid username
-     * 
-     * @return void
-     */
-    public function testInvalidPassword()
-    {
-        $response = $this->_executeApi(
-            array(
-                'user' => 'me',
-                
-            )
-        );
-        $this->assertEquals(
-            array(
-                'result' => 0,
-                'resultDescription' => 'Failure',
-                'reason' => \API\Services\Auth::INVALID_PASSWORD_EMPTY
-            ),
+                'result' => \ResponseController::RESULT_SUCCESS,
+                'resultDescription' => \ResponseController::RESULT_SUCCESS_TEXT,
+                'data' => array(
+                    'ID' => '4',
+                    'Name' => 'CVE-1999-0004',
+                    'Status' => 'Candidate',
+                    'Description' => 'MIME buffer overflow in email clients, e.g. Solaris mailtool and Outlook.',
+                    'References' => 'CERT:CA-98.10.mime_buffer_overflows   |   XF:outlook-long-name   |   SUN:00175   |   MS:MS98-008   |   URL:http://www.microsoft.com/technet/security/bulletin/ms98-008.asp',
+                    'Phase' => 'Modified (19990621-01)',
+                    'Votes' => '   ACCEPT(8) Baker, Cole, Collins, Dik, Landfield, Magdych, Northcutt, Wall  |     MODIFY(1) Frech  |     NOOP(1) Christey  |     REVIEWING(1) Shostack',
+                    'Comments' => ' Frech> Extremely minor, but I believe e-mail is the correct term. (If you reject  |     this suggestion, I will not be devastated.) :-)  |   Christey> This issue seems to have been rediscovered in  |     BUGTRAQ:20000515 Eudora Pro & Outlook Overflow - too long filenames again  |     http://marc.theaimsgroup.com/?l=bugtraq&m=95842482413076&w=2  |       |     Also see  |     BUGTRAQ:19990320 Eudora Attachment Buffer Overflow  |     http://marc.theaimsgroup.com/?l=bugtraq&m=92195396912110&w=2  |   Christey>   |     CVE-2000-0415 may be a later rediscovery of this problem  |     for Outlook.  |   Dik> Sun bug 4163471,  |   Christey> ADDREF BID:125  |   Christey> BUGTRAQ:19980730 Long Filenames & Lotus Products  |     URL:http://marc.theaimsgroup.com/?l=bugtraq&m=90221104526201&w=2',
+                ),
+            ),  
             json_decode((string)$response, true)
         );
         
