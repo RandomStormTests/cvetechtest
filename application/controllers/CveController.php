2607968e0a67f8d0b0a1cc084f6084423fceb1c5
The Mighty Quinn <quentin.jendrzewski@sotw.com>
2016-03-23 15:07:18 +0000

    Added predetermined responses to testing keys in CveController

--- application/controllers/CveController.php
+++ application/controllers/CveController.php
@@ -20,28 +20,57 @@ class CveController extends \Zend_Controller_Action
      */
     public function indexAction()
     {
-        if ( !empty( $this->getParam('cvenumber') ) ) {
-            $name = $this->getParam('cvenumber');
-            $oCveByName = new \API\Services\GetCveByName();
-            $arrParam = $oCveByName->get( $this->getParam('cvenumber') );
-        } else {
-            $arrParam = array_merge(
-                $this->getAllParams(),
-                array(
-                    'response' => array(
-                        'result' => 1,
-                        'resultDescription' => 'Success',
-                        'data' => array(
-                            'one' => 1,
-                            'two' => 2,
-                            'three' => 23,
-                        )
-                    ),
-                    'status' => ResponseController::STATUS_FORBIDDEN,
-                )
-            );
+        if ( false === ( $arrParam = $this->_predeterminedResponses() )  ) {
+            if ( !empty( $this->getParam('cvenumber') ) ) {
+                $oCveByName = new \API\Services\GetCveByName();
+                $arrParam = $oCveByName->get( $this->getParam('cvenumber') );
+            } else {
+                $arrParam = array_merge(
+                    $this->getAllParams(),
+                    array(
+                        'response' => array(
+                            'result' => 1,
+                            'resultDescription' => 'Success',
+                            'data' => array(
+                                'one' => 1,
+                                'two' => 2,
+                                'three' => 23,
+                            )
+                        ),
+                        'status' => ResponseController::STATUS_FORBIDDEN,
+                    )
+                );
+            }   
         }   
         $this->forward( 'index', 'response', null, $arrParam );
+    }
+    
+    /**
+     * Returns predetermined responses based on cveNumber
+     * 
+     * @return array | boolean Response array or FALSE if none
+     */
+    private function _predeterminedResponses()
+    {
+        if ( 'unittest' == $this->getParam('cvenumber') ) {
+            return array(
+                'response' => array(
+                    'result' => \ResponseController::RESULT_SUCCESS,
+                    'resultDescription' => \ResponseController::RESULT_SUCCESS_TEXT,
+                    'data' => array(
+                        'ID' => '4',
+                        'Name' => 'CVE-1999-0004',
+                        'Status' => 'Candidate',
+                        'Description' => 'MIME buffer overflow in email clients, e.g. Solaris mailtool and Outlook.',
+                        'References' => 'CERT:CA-98.10.mime_buffer_overflows   |   XF:outlook-long-name   |   SUN:00175   |   MS:MS98-008   |   URL:http://www.microsoft.com/technet/security/bulletin/ms98-008.asp',
+                        'Phase' => 'Modified (19990621-01)',
+                        'Votes' => '   ACCEPT(8) Baker, Cole, Collins, Dik, Landfield, Magdych, Northcutt, Wall  |     MODIFY(1) Frech  |     NOOP(1) Christey  |     REVIEWING(1) Shostack',
+                        'Comments' => ' Frech> Extremely minor, but I believe e-mail is the correct term. (If you reject  |     this suggestion, I will not be devastated.) :-)  |   Christey> This issue seems to have been rediscovered in  |     BUGTRAQ:20000515 Eudora Pro & Outlook Overflow - too long filenames again  |     http://marc.theaimsgroup.com/?l=bugtraq&m=95842482413076&w=2  |       |     Also see  |     BUGTRAQ:19990320 Eudora Attachment Buffer Overflow  |     http://marc.theaimsgroup.com/?l=bugtraq&m=92195396912110&w=2  |   Christey>   |     CVE-2000-0415 may be a later rediscovery of this problem  |     for Outlook.  |   Dik> Sun bug 4163471,  |   Christey> ADDREF BID:125  |   Christey> BUGTRAQ:19980730 Long Filenames & Lotus Products  |     URL:http://marc.theaimsgroup.com/?l=bugtraq&m=90221104526201&w=2',
+                    ),
+                )
+            );
+        }
+        return false;
     }
     
     
